c2bfda692edd00ebc1464bb57fdb59cde0d1f767
Supriyo Sinha <48999892+Supriyo1998@users.noreply.github.com>
2020-01-26 13:14:40 +0530

    Update NodeMCU_Flame.ino

--- NodeMCU_Flame.ino
+++ NodeMCU_Flame.ino
@@ -8,15 +8,12 @@ int flame_sensor = A0;
 int flame_detected;
 const int threshold=200;
 
-//const char* ssid = "TP-Link_F744";
-//const char* password = "00264235";
 
-const char* ssid = "Airtel-E5573-E0FE";
-const char* password = "sinha@123";
+const char* ssid = "Airtel-E5573-E0FE";//your wifi name
+const char* password = "sinha@123";//your wifi password
 
-//int ledPin = D1;
 
-String apiKey = "VPHULBLKNJ1CQKS5";
+String apiKey = "VPHULBLKNJ1CQKS5"; //your thingspeak api key
 const char* server = "api.thingspeak.com";
 WiFiClient client;
 
@@ -33,8 +30,6 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected");
-
-  //pinMode(ledPin, OUTPUT);
   pinMode(flame_sensor, INPUT);
 }
 
@@ -46,31 +41,12 @@ void loop() {
     {
       Serial.println((String)"Threshold Value at normal: "+flame_detected);
       Serial.println("No flame detected. stay cool");
-      
-      //digitalWrite(ledPin, HIGH);
-      /*String postStr = apiKey;
-      postStr +="&field1=";
-      postStr += String(0);
-      postStr += "\r\n\r\n";
-   
-      client.print("POST /update HTTP/1.1\n");
-      client.print("Host: api.thingspeak.com\n");
-      client.print("Connection: close\n");
-      client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
-      client.print("Content-Type: application/x-www-form-urlencoded\n");
-      client.print("Content-Length: ");
-      client.print(postStr.length());
-      client.print("\n\n");
-      client.print(postStr);
-      delay(15000);
-      Serial.println("Waiting...");*/
     }
     else
     {
       Serial.println(flame_detected);
       Serial.println("Flame detected...! take action immediately.");
-      //digitalWrite(ledPin, LOW);
-      //delay(200);
+     
   
       String postStr = apiKey;
       postStr +="&field1=";
